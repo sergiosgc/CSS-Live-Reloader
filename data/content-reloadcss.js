4f20c2f7ebebf7edb42ab2169b658c38b157797e
Sergio Carvalho <sergio.carvalho@portugalmail.net>
2013-12-01 19:35:03 +0000

    Support for update notification via WebSockets

--- data/content-reloadcss.js
+++ data/content-reloadcss.js
@@ -1,6 +1,4 @@
-(function () {
-
-
+self.port.on('csslivereload', function () {
  let cleanup = function() {
      this.parentNode.removeChild(this);
  }
@@ -12,11 +10,31 @@
     href += href.indexOf('?')<0 ? '?' : '&';
     href += 'livecsscachebuster=' + Date.now();
 
-    candidates[i].href = href;
- /*   let newStylesheet = candidates[i].cloneNode(true);
+//    candidates[i].href = href;
+    let newStylesheet = candidates[i].cloneNode(true);
     newStylesheet.href = href;
-    candidates[i].parentNode.insertBefore(newStylesheet, candidates[i]);
-    window.setTimeout(cleanup.bind(candidates[i]), 200);
-    */
+    if (candidates[i].nextSibling) {
+        candidates[i].parentNode.insertBefore(newStylesheet, candidates[i].nextSibling);
+    } else {
+        candidates[i].parentNode.appendChild(newStylesheet);
+    }
+    window.setTimeout(cleanup.bind(candidates[i]), 500);
  }
-})();
+ if (!window.csslivereloadNotificationSocket) {
+     console.log('connecting');
+    let host = document.location.host == '' ? 'localhost' : document.location.host;
+    window.csslivereloadNotificationSocket = new WebSocket('ws://' + host + ':3000/csslivereload');
+    window.csslivereloadNotificationSocket.onmessage = function(event) {
+        self.port.emit('csslivereload', true);
+    };
+    window.csslivereloadNotificationSocket.onopen = function() {
+    };
+    window.csslivereloadNotificationSocket.onclose = function() {
+        delete window.csslivereloadNotificationSocket;
+    };
+    window.csslivereloadNotificationSocket.onerror = function(error) {
+        window.console.log("Error connecting websocket: " + error);
+        delete window.csslivereloadNotificationSocket;
+    };
+ }
+});
